3915eb90ec4b12768d65d43a2a02916a26cb89ee
AllenC02 <chouallen09@gmail.com>
2023-07-13 00:50:09 +0200

    page 1 done

--- Week_1/Day_3/homework/exercises.js
+++ Week_1/Day_3/homework/exercises.js
@@ -21,3 +21,58 @@ function printFibonacci(length) {
 }
 // printFibonacci(20)
 
+function printArrayAvg(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    let avg = sum / array.length;
+    console.log(avg)
+}
+// printArrayAvg([2,3,4,5,6])
+
+function printArrayMax(array) {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i]
+        }
+    }
+    console.log(max)
+}
+// printArrayMax([9,3,5,1,16,3])
+
+function printArrayMin(array) {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+        }
+    }
+    console.log(min)
+}
+// printArrayMin([9,3,5,1,16,3])
+
+function returnVowels(string) {
+    let vowels = 'aeiou'
+    let result = ''
+    for (let i =0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            result += string[i];
+        }
+    }
+    return result;
+}
+// console.log(returnVowels('aeiouacbauwdnacowqijnqwe'))
+
+function addTwoNum(num1, num2) {
+    result = num1 + num2;
+    return result;
+}
+// console.log(addTwoNum(3,19))
+
+function multTwoNum(num1, num2) {
+    result = num1 * num2;
+    return result;
+}
+console.log(multTwoNum(3,19))
